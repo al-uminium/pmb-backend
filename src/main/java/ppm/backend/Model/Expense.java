fd54bee6073ca4565724ab8707a25f1ec76b8c80
al-uminium <aeronafic@gmail.com>
2024-06-17 19:57:08 +0800

    added getters and setters

--- src/main/java/ppm/backend/Model/Expense.java
+++ src/main/java/ppm/backend/Model/Expense.java
@@ -8,4 +8,29 @@ public class Expense {
   Integer totalCost;
   Map<User, Double> expenseSplit;
   UUID eid;
+  
+  public User getExpenseOwner() {
+    return expenseOwner;
+  }
+  public void setExpenseOwner(User expenseOwner) {
+    this.expenseOwner = expenseOwner;
+  }
+  public Integer getTotalCost() {
+    return totalCost;
+  }
+  public void setTotalCost(Integer totalCost) {
+    this.totalCost = totalCost;
+  }
+  public Map<User, Double> getExpenseSplit() {
+    return expenseSplit;
+  }
+  public void setExpenseSplit(Map<User, Double> expenseSplit) {
+    this.expenseSplit = expenseSplit;
+  }
+  public UUID getEid() {
+    return eid;
+  }
+  public void setEid(UUID eid) {
+    this.eid = eid;
+  }
 }
